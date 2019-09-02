cf0f8d545cb0d33a4771a19b756bcd4f2f20aff9
Shahrukh Md Adil <shahrukh@Shahrukhs-MacBook-Pro.local>
2019-09-02 12:55:22 +0530

    Updates

---
+++ precache-manifest.6301b1535172763c279b153b9d19b0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3d1d1df079a2ba336b88295701340a",
+    "url": "/investo/index.html"
+  },
+  {
+    "revision": "61c3637c7c670e0275c3",
+    "url": "/investo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "50f3f864db427ad861a2",
+    "url": "/investo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "61c3637c7c670e0275c3",
+    "url": "/investo/static/js/2.a67d4524.chunk.js"
+  },
+  {
+    "revision": "50f3f864db427ad861a2",
+    "url": "/investo/static/js/main.1cb3107b.chunk.js"
+  },
+  {
+    "revision": "84d3e1ae2ecd743fca93",
+    "url": "/investo/static/js/runtime~main.aada3e19.js"
+  }
+]);
